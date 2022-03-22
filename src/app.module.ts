550af95eeb9124fe229a4da4bf310aec94e48547
raulanacleto <raul@apipass.com.br>
2022-03-21 21:01:20 -0300

    added mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
+const MONGOOSE_USER =
+  'mongodb+srv://raul:ktoqNFiaYxVnKBC3@cluster0.k4w1w.mongodb.net/smartracking?retryWrites=true&w=majority';
+
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(MONGOOSE_USER, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
